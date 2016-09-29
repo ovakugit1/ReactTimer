082e925aaf409026553821baa6e5c78a462869f7
ovakugit1 <rugocip@stexsy.com>
2016-09-29 13:11:31 +0300

    Update on karma and webpack

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
 var webpackConfig = require('./webpack.config.js');
-webpackConfig.entry = {};
+webpackConfig.entry = ['script!jquery/dist/jquery.min.js'];
 var browser = process.env.TRAVIS ? 'Firefox' : 'Chrome'; // No chrome support on travis ci.
 module.exports = function (config) {
   config.set({
